fc0f2c00b7e23ddcd6a478c6467907f8d4220371
leolegPremiumPlus <117438813+leolegPremiumPlus@users.noreply.github.com>
2024-09-11 20:34:13 +0200

    Delete tutorial_bot-master directory

--- tutorial_bot-master/src/models/guildStats.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const guildStats = new Schema({
-    guild: String,
-    members: String,
-})
-
-module.exports = model("Guild_Stats", guildStats);
--- tutorial_bot-master/src/models/userConfig.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const userConfig = new Schema({
-    user: String,
-    pokemons: [String],
-    badges:[String]
-})
-
-module.exports = model("User_Pokemons", userConfig);
